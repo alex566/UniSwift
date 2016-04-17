f3e4e2de778285cea260b794037e2a5c98b81530
Alexey Oleynik <alexei566@ukr.net>
2016-04-17 04:16:02 +0300

    start

---
+++ UniSwift/Assets/Plugins/UniSwift/Editor/SwiftScriptEditor.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+using System.Diagnostics;
+
+[CustomEditor(typeof(SwiftScript))]
+public class SwiftScriptEditor : Editor {
+
+	private SwiftScript targetScript;
+
+	void OnEnable () {
+		targetScript = target as SwiftScript;
+	}
+
+	public override void OnInspectorGUI() {
+		targetScript.TargetClassName = EditorGUILayout.TextField ("Target class name", targetScript.TargetClassName);
+
+		if (targetScript.TargetClassName == "") {
+			targetScript.TargetClassName = "SwiftClass";
+		}
+			
+		EditorGUILayout.SelectableLabel(targetScript.SwiftCode, GUILayout.MaxHeight(75));
+
+		if (GUILayout.Button("Edit")) {
+			EditScript ();
+		}
+	}
+
+	#region
+
+	private string tempSwiftFile;
+	private FileSystemWatcher watcher;
+
+	private void EditScript() {
+		if (!TempFileExist()) {
+			CreateTempFile ();
+			CreateFileWatcher ();
+		}
+
+		Process.Start(tempSwiftFile);
+	}
+
+	private bool TempFileExist() {
+		return tempSwiftFile != null && File.Exists (tempSwiftFile);
+	}
+
+	private void CreateTempFile() {
+		tempSwiftFile = string.Format("{0}.swift", FileUtil.GetUniqueTempPathInProject ());
+
+		File.WriteAllText(tempSwiftFile, targetScript.SwiftCode);
+	}
+
+	private void CreateFileWatcher() {
+		watcher = new FileSystemWatcher (tempSwiftFile);
+		watcher.Changed += (object sender, FileSystemEventArgs e) => {
+			targetScript.SwiftCode = File.ReadAllText(tempSwiftFile);
+		};
+	}
+
+	#endregion
+}
---
+++ UniSwift/Assets/Plugins/UniSwift/Scripts/SwiftScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwiftScript : MonoBehaviour {
+
+	public string TargetClassName;
+
+	public string SwiftCode;
+
+}
